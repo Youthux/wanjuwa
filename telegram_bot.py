ed46fd749c6c96aea0f76278697c0c7637666b83
Jayson Albert <momeijw@gmail.com>
2022-09-18 13:46:54 +0800

    优化交互

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,9 +10,9 @@ from activity import *
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging.INFO)
+                    level=logging.INFO)
 
-REQUEST_KWARGS={
+REQUEST_KWARGS = {
     'proxy_url': 'socks5h://127.0.0.1:1080/',
 }
 
@@ -21,37 +21,46 @@ dispatcher = updater.dispatcher
 
 
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-    
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+
 def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-        
+    context.bot.send_message(
+        chat_id=update.effective_chat.id, text=update.message.text)
+
+
 def update_token(update: Update, context: CallbackContext):
     logging.info('收到token: %s', context.args[0])
     context.bot_data['token'] = context.args[0]
-    context.bot.send_message(chat_id=update.effective_chat.id, text='更新成功')   
-    
+    context.bot.send_message(chat_id=update.effective_chat.id, text='更新成功')
+
+
 def activity(update: Update, context: CallbackContext):
     try:
         token = context.bot_data['token']
     except KeyError:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='请先设置token')
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text='请先设置token')
         return
     userId = context.args[0]
     if not userId:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='请输入要查询的用户Id')
-        
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text='请输入要查询的用户Id')
+
     try:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text='查询中请稍后...')
         title = find_activity(token, userId)
-        context.bot.send_message(chat_id=update.effective_chat.id, text='{}报名了活动：{}'.format(userId, title))
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text='{}报名了活动：{}'.format(userId, title))
     except NotFound as e:
         context.bot.send_message(chat_id=update.effective_chat.id, text=e.msg)
     except TokenInvalid as e:
-        context.bot.send_message(chat_id=update.effective_chat.id, text='{},请重新输入token'.format(e.msg))
-        
-        
-    
-    
+        context.bot.send_message(
+            chat_id=update.effective_chat.id, text='{},请重新输入token'.format(e.msg))
+
+
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
