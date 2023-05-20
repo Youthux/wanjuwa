6cae7fe71e1f96dcf63b71de8b563aff95c75a4b
Jayson Albert <momeijw@gmail.com>
2023-05-20 17:23:29 +0800

    更新bot api 版本

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,48 +3,41 @@ from telegram import Update
 from config import token
 from telegram.ext import CallbackContext
 from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler, Filters
+from telegram.ext import MessageHandler, filters
 from activity import *
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-REQUEST_KWARGS = {
-    'proxy_url': 'socks5h://127.0.0.1:1080/',
-}
-
-updater = Updater(token=token, use_context=True, request_kwargs=REQUEST_KWARGS)
-dispatcher = updater.dispatcher
-
-
-def start(update: Update, context: CallbackContext):
-    context.bot.send_message(
+async def start(update: Update, context: CallbackContext):
+    await context.bot.send_message(
         chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
-def echo(update: Update, context: CallbackContext):
-    context.bot.send_message(
+async def echo(update: Update, context: CallbackContext):
+    await context.bot.send_message(
         chat_id=update.effective_chat.id, text=update.message.text)
 
 
-def update_token(update: Update, context: CallbackContext):
+async def update_token(update: Update, context: CallbackContext):
     logging.info('收到token: %s', context.args[0])
     context.bot_data['token'] = context.args[0]
-    context.bot.send_message(chat_id=update.effective_chat.id, text='更新成功')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text='更新成功')
 
 
-def activity(update: Update, context: CallbackContext):
+async def activity(update: Update, context: CallbackContext):
     try:
         token = context.bot_data['token']
     except KeyError:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text='请先设置token')
         return
     try:
         userId = context.args[0]
     except IndexError:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text='请输入要查询的用户Id')
         return
     try:
@@ -53,33 +46,40 @@ def activity(update: Update, context: CallbackContext):
         rows = 100
 
     try:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text='搜索最近{}场活动中，请稍后...'.format(rows))
 
         title = find_activity(token, userId, rows)
         for t in title:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=t)
+           await context.bot.send_message(chat_id=update.effective_chat.id, text=t)
     except TokenInvalid as e:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text='{},请重新输入token'.format(e.msg))
     except requests.exceptions.ConnectionError as e:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text='连接超时，请重新查询')
     except Exception as e:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text='查询失败'.format(e))
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(token).build()
+    
+    REQUEST_KWARGS = {
+        'proxy_url': 'socks5h://127.0.0.1:1080/',
+    }
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    application.add_handler(echo_handler)
 
-update_handler = CommandHandler('token', update_token)
-dispatcher.add_handler(update_handler)
+    update_handler = CommandHandler('token', update_token)
+    application.add_handler(update_handler)
 
-activity_handler = CommandHandler('activity', activity)
-dispatcher.add_handler(activity_handler)
+    activity_handler = CommandHandler('activity', activity)
+    application.add_handler(activity_handler)
 
-updater.start_polling()
+    application.run_polling()
